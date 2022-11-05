4d9b03706489efbd536c0c4f84679ae6d3b1690a
Minsu <syt07100@gmail.com>
2022-11-05 20:58:26 +0900

    Initialize project

---
+++ src/main/java/com/example/Recore_Back/RecoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Recore_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoreBackApplication.class, args);
+	}
+
+}
